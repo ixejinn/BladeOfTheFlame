e81cd8a9d590236aa9aa9ea53e8826b9f2318833
ixejinn <174031282+ixejinn@users.noreply.github.com>
2024-08-30 05:19:23 +0900

    Feat: Collision (OBB)
    Collision (OBB)
    Refactor Collider & CollisionManager

--- Blade-of-the-Flame/State/SampleSave.cpp
+++ Blade-of-the-Flame/State/SampleSave.cpp
@@ -6,7 +6,7 @@
 
 void SampleSave::Init()
 {
-	GameObject* obj = GameObjectManager::GetInstance().CreateObject("TestObj");
+	/*GameObject* obj = GameObjectManager::GetInstance().CreateObject("TestObj");
 
 	obj->AddComponent<Transform>();
 	obj->GetComponent<Transform>()->SetScale({ 100, 100 });
@@ -26,17 +26,38 @@ void SampleSave::Init()
 	obj->AddComponent<Audio>();
 	obj->GetComponent<Audio>()->SetAudio("Assets/bouken.mp3");
 
-	obj->AddComponent<Player>();
+	obj->AddComponent<Player>();*/
 
-	GameObject* square = GameObjectManager::GetInstance().CreateObject("square");
+	GameObject* square1 = GameObjectManager::GetInstance().CreateObject("squar1e");
 
-	square->AddComponent<Transform>();
-	square->GetComponent<Transform>()->SetScale({ 100, 100 });
+	square1->AddComponent<Transform>();
+	square1->GetComponent<Transform>()->SetScale({ 100, 100 });
 
-	square->AddComponent<Sprite>();
-	square->GetComponent<Sprite>()->SetColor({ 200, 100, 20 });
+	square1->AddComponent<Sprite>();
+	square1->GetComponent<Sprite>()->SetColor({ 200, 100, 20 });
 
-	//GameObjectManager::GetInstance().RemoveObject("square");
+	square1->AddComponent<PlayerController>();
+	PlayerController* pCtrl = square1->GetComponent<PlayerController>();
+	pCtrl->SetRotKeys(PlayerController::LEFT, AEVK_Q);
+	pCtrl->SetRotKeys(PlayerController::RIGHT, AEVK_E);
+
+	square1->AddComponent<RigidBody>();
+
+	square1->AddComponent<BoxCollider>();
+	square1->GetComponent<BoxCollider>()->SetType(Collider::OBB_TYPE);
+
+	GameObject* square2 = GameObjectManager::GetInstance().CreateObject("square2");
+
+	square2->AddComponent<Transform>();
+	square2->GetComponent<Transform>()->SetScale({ 100, 100 });
+	square2->GetComponent<Transform>()->SetPosition({ 300, 300 });
+
+	square2->AddComponent<Sprite>();
+	square2->GetComponent<Sprite>()->SetColor({ 50, 100, 150 });
+
+	square2->AddComponent<RigidBody>();
+
+	square2->AddComponent<BoxCollider>();
 }
 
 void SampleSave::Update()
